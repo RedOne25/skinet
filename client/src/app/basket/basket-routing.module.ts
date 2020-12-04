6756b9ef9d3bd6d7dbfc81f7b140dc486439c386
Mohammed Radhwane BENLATRECHE <benlatreche.radhwane@gmail.com>
2020-12-05 00:51:47 +0100

    End of section 14

---
+++ client/src/app/basket/basket-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BasketComponent } from './basket.component';
+import { RouterModule, Routes } from '@angular/router';
+import { BasketModule } from './basket.module';
+
+const routes: Routes = [
+  {path: '', component: BasketComponent}
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
+})
+export class BasketRoutingModule { }
